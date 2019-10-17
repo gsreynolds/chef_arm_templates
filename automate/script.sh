f846cd0f2f7a7e67007d29f56a063ada989c2c35
Gavin Reynolds <gavin@chef.io>
2019-10-17 14:26:14 +0100

    Add fqdn and license arguments to automate script
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- automate/script.sh
+++ automate/script.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+FQDN="${1:-}"
+LICENSE="${2:-}"
+
 firewall-cmd --zone=public --permanent --add-service=https
 firewall-cmd --reload
 
@@ -7,7 +15,7 @@ sudo unzip chef-automate_linux_amd64.zip
 echo vm.max_map_count=262144 | sudo tee -a /etc/sysctl.conf
 echo vm.dirty_expire_centisecs=20000 | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p /etc/sysctl.conf
-# sudo ./chef-automate init-config --fqdn ""
+sudo ./chef-automate init-config --fqdn "$FQDN"
 sudo ./chef-automate init-config
 sudo ./chef-automate deploy --channel current --upgrade-strategy none --accept-terms-and-mlsa config.toml
-# sudo chef-automate license apply ""
+sudo chef-automate license apply "$LICENSE"
